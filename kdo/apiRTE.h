e4927c9f550b0d7ec1bc1e03c7d12b70a0e1c709
felixjosquin <felix.josquin@depinfonancy.net>
2023-09-02 20:45:52 +0200

    creat apiRTE file + add getToken

---
+++ kdo/apiRTE.h
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#ifndef API_RTE_H
+  #define API_RTE_H
+
+  extern String getToken();
+  extern String token;
+#endif
