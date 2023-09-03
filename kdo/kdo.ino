5d958a16e1292b2b2d4ff10b74574dc2045a6cba
felixjosquin <felix.josquin@depinfonancy.net>
2023-09-03 21:20:41 +0200

    revert

--- kdo/kdo.ino
+++ kdo/kdo.ino
@@ -3,103 +3,106 @@
 #include <ArduinoJson.h>
 #include <Adafruit_NeoPixel.h>
 #include "secret.h"
-#include "apiRTE.h"
 
 #define PIN 14
 #define NUMPIXELS 10
-#define NB_PRODUCTION_TYPE 9
+#define NB_PRODUCTION_TYPE 10
 
-const char* productionTypeList[NB_PRODUCTION_TYPE] = { "\"FOSSIL_OIL\"", "\"FOSSIL_GAS\"", "\"HYDRO\"", "\"BIOENERGY\"", "\"NUCLEAR\"", "\"FOSSIL_HARD_COAL\"", "\"WIND\"", "\"PUMPING\"", "\"SOLAR\"" };
-const int productionCO2[NB_PRODUCTION_TYPE] = { 730, 418, 6, 494, 6, 1060, 14,  10, 43 };                        // geCO2/kWh
-const unsigned long meanProduction[NB_PRODUCTION_TYPE] = { 688, 2356, 5046, 446, 34274, 1374, 1020,  0, 185 };  // MW
+const char* productionTypeList[NB_PRODUCTION_TYPE] = { "\"FOSSIL_OIL\"", "\"FOSSIL_GAS\"", "\"HYDRO\"", "\"BIOENERGY\"", "\"NUCLEAR\"", "\"FOSSIL_HARD_COAL\"", "\"WIND\"", "\"EXCHANGE\"", "\"PUMPING\"", "\"SOLAR\"" };
+const int productionCO2[NB_PRODUCTION_TYPE] = { 730, 418, 6, 494, 6, 1060, 14, 420, 10, 43 };                        // geCO2/kWh
+const unsigned long meanProduction[NB_PRODUCTION_TYPE] = { 688, 2356, 5046, 446, 34274, 1374, 1020, 2389, 0, 185 };  // MW
 const int first_color[3] = { 0, 125, 125 };
 const int last_color[3] = { 255, 0, 125 };
 
+String token = "";
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 int** color_pixels = new int*[NUMPIXELS];
 
 void setup() {
-  // init_colors();
-  // pixels.begin();
+  init_colors();
+  pixels.begin();
   Serial.begin(9600);
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PASSWORD);
   Serial.println("Start to connect");
-  // int coef = 1;
-  // int j = 0;
+  int coef = 1;
+  int j = 0;
   while (!WiFi.isConnected()) {
     Serial.print(".");
-    // clear_colors();
-    // pixels.setPixelColor(j, pixels.Color(color_pixels[j][0], color_pixels[j][1], color_pixels[j][2]));
-    // pixels.show();
-    // j = j + coef;
-    // if (j + 1 == NUMPIXELS) { coef = -1; }
-    // if (j == 0) { coef = 1; }
+    clear_colors();
+    pixels.setPixelColor(j, pixels.Color(color_pixels[j][0], color_pixels[j][1], color_pixels[j][2]));
+    pixels.show();
+    j = j + coef;
+    if (j + 1 == NUMPIXELS) { coef = -1; }
+    if (j == 0) { coef = 1; }
     delay(100);
   }
-  // clear_colors();
-  // pixels.show();
-  Serial.println();
-  Serial.println(getToken());  
+  clear_colors();
+  pixels.show();
+  Serial.println("sucess");
+  HTTPClient http;
+  WiFiClientSecure client;
+  client.setInsecure();
+  getToken(&client, &http);
 }
 
 void loop() {
-  // HTTPClient http;
-  // WiFiClientSecure client;
-  // client.setInsecure();
+  HTTPClient http;
+  WiFiClientSecure client;
+  client.setInsecure();
 
-  // float total = 0;
-  // float eqCO2 = 0;
-  // bool sucess;
-  // unsigned long* productionElec = new unsigned long[NB_PRODUCTION_TYPE];
+  float total = 0;
+  float eqCO2 = 0;
+  bool sucess;
+  unsigned long* productionElec = new unsigned long[NB_PRODUCTION_TYPE];
 
-  // for (int r = 0; r < NB_PRODUCTION_TYPE; r++) {
-  //   productionElec[r] = 80000;
-  // }
-  // sucess = getData(&client, &http, productionElec);
-  // for (int k = 0; k < NB_PRODUCTION_TYPE; k++) {
-  //   if (productionElec[k] >= 80000) {
-  //     productionElec[k] = meanProduction[k];
-  //   }
-  //   total += productionElec[k] / 1000;
-  // }
-  // for (int k = 0; k < NB_PRODUCTION_TYPE; k++) {
-  //   eqCO2 += productionElec[k] * productionCO2[k] / (total * 1000);
-  // }
-  // Serial.print("\nEquivalent CO2");
-  // Serial.print(" : ");
-  // Serial.println(eqCO2);
+  for (int r = 0; r < NB_PRODUCTION_TYPE; r++) {
+    productionElec[r] = 80000;
+  }
+  sucess = getData(&client, &http, productionElec);
+  for (int k = 0; k < NB_PRODUCTION_TYPE; k++) {
+    if (productionElec[k] >= 80000) {
+      productionElec[k] = meanProduction[k];
+    }
+    total += productionElec[k] / 1000;
+  }
+  for (int k = 0; k < NB_PRODUCTION_TYPE; k++) {
+    eqCO2 += productionElec[k] * productionCO2[k] / (total * 1000);
+  }
+  Serial.print("\nEquivalent CO2");
+  Serial.print(" : ");
+  Serial.println(eqCO2);
 
-  // clear_colors();
-  // for (int i = 0; i < NUMPIXELS; i++) {
-  //   int d = 40;
-  //   if (i == 0) { d = 30; }
-  //   if (i + 1 == NUMPIXELS) { d = 50; }
-  //   if (eqCO2 > d + 10 * i) {
-  //     pixels.setPixelColor(i, pixels.Color(color_pixels[i][0], color_pixels[i][1], color_pixels[i][2]));
-  //     pixels.show();
-  //     delay(200);
-  //   }
-  // }
-  // if (!sucess) {
-  //   show_error();
-  // }
-  // delay(12 * 60 * 1000);
+  clear_colors();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    int d = 40;
+    if (i == 0) { d = 30; }
+    if (i + 1 == NUMPIXELS) { d = 50; }
+    if (eqCO2 > d + 10 * i) {
+      pixels.setPixelColor(i, pixels.Color(color_pixels[i][0], color_pixels[i][1], color_pixels[i][2]));
+      pixels.show();
+      delay(200);
+    }
+  }
+  if (!sucess) {
+    show_error();
+  }
+  delay(12 * 60 * 1000);
 }
 
-// void getToken(WiFiClientSecure* client, HTTPClient* http) {
-//   http->begin(*client, "https://digital.iservices.rte-france.com/token/oauth");
-//   http->addHeader("Content-Type", "application/x-www-form-urlencoded");
-//   http->addHeader("Authorization", secret_api_rte);
-//   int httpCode = http->POST("");
-//   if (httpCode == 200) {
-//     StaticJsonDocument<192> tokenDoc;
-//     deserializeJson(tokenDoc, http->getString());
-//     const char* accessToken = tokenDoc["access_token"];
-//     token = String(accessToken);
-//   }
-//   http->end();
-// }
+void getToken(WiFiClientSecure* client, HTTPClient* http) {
+  http->begin(*client, "https://digital.iservices.rte-france.com/token/oauth");
+  http->addHeader("Content-Type", "application/x-www-form-urlencoded");
+  http->addHeader("Authorization", secret_api_rte);
+  int httpCode = http->POST("");
+  if (httpCode == 200) {
+    StaticJsonDocument<192> tokenDoc;
+    deserializeJson(tokenDoc, http->getString());
+    const char* accessToken = tokenDoc["access_token"];
+    token = String(accessToken);
+  }
+  http->end();
+}
 
 bool getData(WiFiClientSecure* stream, HTTPClient* http, unsigned long* res) {
   String key;
@@ -108,7 +111,7 @@ bool getData(WiFiClientSecure* stream, HTTPClient* http, unsigned long* res) {
   bool sucess = true;
 
   String header = "Bearer " + token;
-  http->begin(*stream, "https://digital.iservices.rte-france.com/open_api/actual_generation/v1/generation_mix_15min_time_scale?production_subtype=TOTAL&start_date=2022-08-01T00:00:00+02:00&end_date=2022-08-02T02:16:00+02:00");
+  http->begin(*stream, "https://digital.iservices.rte-france.com/open_api/actual_generation/v1/generation_mix_15min_time_scale?production_subtype=TOTAL");
   http->addHeader("Authorization", header);
   int httpCode = http->GET();
   Serial.println(httpCode);
@@ -168,7 +171,7 @@ bool getData(WiFiClientSecure* stream, HTTPClient* http, unsigned long* res) {
   } else if (httpCode == 401) {
     http->end();
     Serial.println("Unauthorize get token and resend");
-    // getToken(stream, http);
+    getToken(stream, http);
     sucess = getData(stream, http, res);
   }
   return sucess;
--- kdo/secret.h
+++ kdo/secret.h
@@ -1,10 +1,9 @@
 #ifndef SECRET_H
-  
-  #define SECRET_H
+#define SECRET_H
 
-  extern const char SSID[];
-  extern const char PASSWORD[];
-  extern const char secret_api_rte[];
+extern const char SSID[];
+extern const char PASSWORD[];
+extern const char secret_api_rte[];
 
 #endif
 
